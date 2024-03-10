c3ded9d9b32fe1f1c5f8200fe577d5480464a2d2
tommy <torreslicet008@gmail.com>
2024-03-10 16:19:22 -0700

    Created React app
    I imported files from Assignment 1 to Assignment 2 files and was able to create react app.

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './App.css'
+
+export default function app(){
+  return(
+    <>
+    <div className="Header">
+<h1>Hi! Hello! Welcome!</h1>
+</div>
+
+<div className="FrontImage">
+<img src="https://pbs.twimg.com/media/F-7sBeaa0AA4l6y?format=jpg&name=medium" alt="three blue dragons" width="300" height="300"/>
+</div>
+
+<div className="FrontImageCaption">
+<h5>this lovely image was created by nagarnia_art!</h5>
+</div>
+
+<h3>Here at CSUSB</h3>
+<p id="borderImage">I'm a third year Computer Science student and a San Bernardino local. Born and lived here since 2002.</p>
+
+
+<h3>Hobbies</h3>
+<p id="borderImage">I'm an amateur photographer in my free time and like to play video games. 
+My most played games at the moment are Genshin Impact and Baldur's Gate 3</p>
+
+
+<h3>And Finally...</h3>
+<p id="borderImage">I adopted an orange female cat named Tonee! She's very cuddly and her favorite food is salmon.</p>
+</>
+)
+}
+
+
+
+
+
